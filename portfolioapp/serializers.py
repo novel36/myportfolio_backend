ad4d3a327a4d84e7414be7e95ee7dcc325c05b14
novel36 <novelwolde36@gmail.com>
2022-09-13 04:33:15 -0700

    added serializer to models

--- portfolioapp/serializers.py
+++ portfolioapp/serializers.py
@@ -1,9 +1,59 @@
 from dataclasses import fields
 from rest_framework import serializers
-from .models import Task
+from .models import Task,Skills,AboutME,ContactMe,Education,MySkill,PersonalSkill,Portfolio,ProfessionalSkill,SocialmediaLinks,Tools
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model= Task
+        fields='__all__'
+
+class SkillsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Skills
+        fields='__all__'
+
+class AboutMESerializer(serializers.ModelSerializer):
+    class Meta:
+        model= AboutME
+        fields='__all__'
+
+class ContactMeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= ContactMe
+        fields='__all__'
+
+class MySkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= MySkill
+        fields='__all__'
+
+class EducationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Education
+        fields='__all__'
+
+class PersonalSkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= PersonalSkill
+        fields='__all__'
+
+class PortfolioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Portfolio
+        fields='__all__'
+
+class ProfessionalSkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= ProfessionalSkill
+        fields='__all__'
+
+class SocialmediaLinksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= SocialmediaLinks
+        fields='__all__'
+
+class ToolsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Tools
         fields='__all__'
         
