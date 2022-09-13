a7d3e39ebbdec7d037f57472494768135a8c63cd
novel36 <novelwolde36@gmail.com>
2022-09-13 04:16:54 -0700

    changet to Django admin  plural model

--- portfolioapp/admin.py
+++ portfolioapp/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import Task
+from .models import Task,AboutME,ContactMe,Education,MySkill,PersonalSkill,Portfolio,ProfessionalSkill,Skills,SocialmediaLinks,Tools
 # Register your models here.
-admin.site.register(Task)
+admin.site.register(AboutME)
+admin.site.register(ContactMe)
+admin.site.register(Education)
+admin.site.register(MySkill)
+admin.site.register(PersonalSkill)
+admin.site.register(Portfolio)
+admin.site.register(ProfessionalSkill)
+admin.site.register(Tools)
+admin.site.register(Skills)
+admin.site.register(SocialmediaLinks)
+
+
--- portfolioapp/models.py
+++ portfolioapp/models.py
@@ -6,6 +6,9 @@ class Task(models.Model):
     title=models.CharField(max_length=200)
     completed=models.BooleanField(default=False)
 
+    class Meta:
+        verbose_name_plural = "Tasks"
+
     def __str__(self):
         return self.title
 
@@ -15,12 +18,18 @@ class Task(models.Model):
 class MySkill(models.Model):
     skillname=models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name_plural = "MySkills"
+
     def __str__(self):
         return self.skillname
 
 class SocialmediaLinks(models.Model):
     socialMediaName=models.CharField(max_length=150)
     link=models.CharField(max_length=500)
+
+    class Meta:
+        verbose_name_plural = "SocialmediaLinks"
 
     def __str__(self):
         return self.socialMediaName
@@ -32,6 +41,9 @@ class AboutME(models.Model):
     photo1=models.ImageField()
     photo2=models.ImageField()
 
+    class Meta:
+        verbose_name_plural = "AboutME"
+
     def __str__(self):
         return self.name
 
@@ -39,11 +51,18 @@ class PersonalSkill(models.Model):
     skillname=models.CharField(max_length=700)
     skill_in_percent=models.IntegerField()
 
+    class Meta:
+        verbose_name_plural = "PersonalSkills"
+
     def __str__(self):
         return self.skillname
 class ProfessionalSkill(models.Model):
     skillname=models.CharField(max_length=700)
     skill_in_percent=models.IntegerField()
+
+
+    class Meta:
+        verbose_name_plural = "ProfessionalSkills"
 
     def __str__(self):
         return self.skillname
@@ -52,6 +71,9 @@ class Skills(models.Model):
     professionalSkill=models.ForeignKey(ProfessionalSkill,on_delete=models.CASCADE)
     personalSkill=models.ForeignKey(PersonalSkill,on_delete=models.CASCADE)
     name=models.CharField(max_length=150)
+
+    class Meta:
+        verbose_name_plural = "Skills"
 
     def __str__(self):
         return self.name
@@ -62,6 +84,9 @@ class Education(models.Model):
     started_year=models.CharField(max_length=150)
     finished_year=models.CharField(max_length=150)
 
+    class Meta:
+        verbose_name_plural = "Education"
+
     def __str__(self):
         return self.schoolName
 
@@ -71,12 +96,18 @@ class Portfolio(models.Model):
     project_images=models.CharField(max_length=150)
     project_link=models.CharField(max_length=150)
 
+    class Meta:
+        verbose_name_plural = "Portfolio"
+
     def __str__(self):
         return self.project_title
 
 class Tools(models.Model):
     tool_name=models.CharField(max_length=200)
     used_for=models.CharField(max_length=500)
+
+    class Meta:
+        verbose_name_plural = "Tools"
 
     def __str__(self):
         return self.tool_name
@@ -85,6 +116,9 @@ class Tools(models.Model):
 class ContactMe(models.Model):
     firstname=models.CharField(max_length=300)
     lasname=models.CharField(max_length=300)
+
+    class Meta:
+        verbose_name_plural = "ContactMe"
 
     def __str__(self):
         return self.firstname
