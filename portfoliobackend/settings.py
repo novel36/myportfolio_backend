5e0dcb8100437d092fc93aec9555256b854b63f4
novel36 <novelwolde36@gmail.com>
2022-09-28 10:55:45 -0700

    .

--- portfoliobackend/settings.py
+++ portfoliobackend/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['novel.up.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
